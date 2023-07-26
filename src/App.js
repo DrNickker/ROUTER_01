cf0bb36e4c44c21c8e8e10eb502434173b56b2ce
DrNickker <mintagirov2015@yandex.ru>
2023-07-26 15:57:32 +0500

    router_dom_reinstall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HeadPage from '../src/components/HeadPage'
 import NotFoundPage from '../src/components/NotFoundPage'
 import CreatePost from '../src/components/CreatePost'
@@ -10,13 +10,13 @@ import EditPage from '../src/components/EditPage'
 export default function App() {
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route path="/" exact component={HeadPage} />
         <Route path="/posts/new" exact component={CreatePost}/>
         <Route path="/posts/:id" exact component={DisplayPage} />
         <Route path="/posts/:id/:edit" exact component={EditPage} />
         <Route path="*" exact component={NotFoundPage} />
-      </Routes>
+      </Switch>
     </Router>
   )
 }
